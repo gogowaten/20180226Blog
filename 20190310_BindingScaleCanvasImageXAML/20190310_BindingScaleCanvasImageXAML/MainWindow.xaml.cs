17978278474ad5aff47775532ff205dec3bb8f9e
waten <gogowaten@yahoo.co.jp>
2019-03-11 14:05:15 +0900

    201903111405

--- 20190310_BindingScaleCanvasImageXAML/20190310_BindingScaleCanvasImageXAML/MainWindow.xaml.cs
+++ 20190310_BindingScaleCanvasImageXAML/20190310_BindingScaleCanvasImageXAML/MainWindow.xaml.cs
@@ -1,27 +1,16 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
 using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.Globalization;
 
 
-//全部XAMLで書こうとしたけどムリ、できない
+
+//BindingのほとんどをXAMLで書いてみた
+//最初はできないかと思った
 //BindingのparameterにImageのWidthを指定する方法がわからない
 //RelativeSourceで渡そうとしたけどImageまででWidthまで届かない感じ
-//MultiBindingも試そうとしたけど、そもそもMultiBindingの書き方がわからないし、真似して書いても入力候補が出ないから間違えそう
-//結論
-//parameterを渡すようなBindingは、XAMLよりC#で書いたほうが早い
-//良かったのは、ImageのScaleのBinding、これはXAMLのほうがラク
+//だったけどMultiBindingでなんとかできた
 
 namespace _20190310_BindingScaleCanvasImageXAML
 {
@@ -33,7 +22,6 @@ namespace _20190310_BindingScaleCanvasImageXAML
         public MainWindow()
         {
             InitializeComponent();
-
             
             //表示する画像ファイルのパス
             string filePath1 = @"D:\ブログ用\チェック用2\NEC_6221_2019_02_24_午後わてん_half.jpg";
@@ -44,32 +32,7 @@ namespace _20190310_BindingScaleCanvasImageXAML
         }
     }
 
-    //Value * parameterを返す
-    public class MyConverter : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return (double)value * (double)parameter;
-        }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
-
-    public class MYmy : IValueConverter
-    {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            return value;
-        }
-
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-        {
-            throw new NotImplementedException();
-        }
-    }
     public class MyMulti : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
--- 20190311_BindingScaleCanvasImage/20190311_BindingScaleCanvasImage/MainWindow.xaml.cs
+++ 20190311_BindingScaleCanvasImage/20190311_BindingScaleCanvasImage/MainWindow.xaml.cs
@@ -1,12 +1,21 @@
 ﻿using System;
-using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace _20190310_BindingScaleCanvasImage
+using System.Globalization;
+
+namespace _20190311_BindingScaleCanvasImage
 {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
@@ -18,9 +27,8 @@ namespace _20190310_BindingScaleCanvasImage
             InitializeComponent();
 
             ContentRendered += MainWindow_ContentRendered;
-
             //表示する画像ファイルのパス
-            string filePath1 = @"D:\ブログ用\チェック用2\NEC_6221_2019_02_24_午後わてん_16colors_trim.png";
+            string filePath1 = @"D:\ブログ用\チェック用2\NEC_6221_2019_02_24_午後わてん_half.jpg";
 
             //Imageに画像表示
             MyImage1.Source = new BitmapImage(new Uri(filePath1));
@@ -29,29 +37,11 @@ namespace _20190310_BindingScaleCanvasImage
         //アプリが表示された直後
         private void MainWindow_ContentRendered(object sender, EventArgs e)
         {
-            //Image拡大表示の補間法指定、今回はニアレストネイバー法
-            RenderOptions.SetBitmapScalingMode(MyImage1, BitmapScalingMode.NearestNeighbor);
-
-            //ScaleTransform作成してImageのRenderTransformに指定
-            //これをしないと拡大できない
-            var st = new ScaleTransform();
-            MyImage1.RenderTransform = st;
-
-
-            //ここからBinding
-            //ソース SliderのValue
-            //ターゲット ImageのScaleTransformのXとY
-            var b = new Binding();
-            b.Source = SliderScale;
-            b.Path = new PropertyPath(Slider.ValueProperty);
-            BindingOperations.SetBinding(st, ScaleTransform.ScaleXProperty, b);
-            BindingOperations.SetBinding(st, ScaleTransform.ScaleYProperty, b);
-
             //ソース Slider.Value
             //ターゲット CanvasのWidth
             //Canvas.Width = Slider.Value * Image.ActualWidthをにするためにConverter指定
-            //ParameterにImage.Width
-            b = new Binding();
+            //ParameterにImage.Width            
+            var b = new Binding();
             b.Source = SliderScale;
             b.Path = new PropertyPath(Slider.ValueProperty);
             b.ConverterParameter = MyImage1.ActualWidth;
@@ -65,7 +55,6 @@ namespace _20190310_BindingScaleCanvasImage
             b.ConverterParameter = MyImage1.ActualHeight;
             b.Converter = new MyConverter();
             MyCanvas1.SetBinding(HeightProperty, b);
-
         }
 
     }
@@ -74,7 +63,7 @@ namespace _20190310_BindingScaleCanvasImage
     public class MyConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-        {            
+        {
             return (double)value * (double)parameter;
         }
 
@@ -83,5 +72,4 @@ namespace _20190310_BindingScaleCanvasImage
             throw new NotImplementedException();
         }
     }
-
 }
