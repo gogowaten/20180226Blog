a7947e82011e72590429a6038dadbcf29a5733ee
waten <gogowaten@yahoo.co.jp>
2019-03-14 10:14:14 +0900

    201903141014

--- 20190312/20190312/MainWindow.xaml.cs
+++ 20190312/20190312/MainWindow.xaml.cs
@@ -31,8 +31,6 @@ namespace _20190312
     public partial class MainWindow : Window
     {
         static int ColorCount = 20;
-        List<MyData> myData = new List<MyData>();
-        MyDatas myDataList;
         Palette Palette1;
         PaletteVer2 PaletteV2;
 
@@ -43,21 +41,16 @@ namespace _20190312
             MyAdd();
             MyButton1.Click += (s, e) =>
             {
-                myData[0].Color = Colors.White;//おk
-                myDataList.MyDatasList[0].Color = Colors.White;//おk
                 Palette1.PaletteColors[0] = Colors.White;
             };
 
             MyButton2.Click += (s, e) =>
             {
-                myData[0] = new MyData() { Color = Colors.Red };//Bindingが外れる
-                myDataList.MyDatasList[0] = new MyData() { Color = Colors.Red };//Bindingが外れる
+
             };
 
             MyButton3.Click += (s, e) =>
             {
-                myData[0].Color = Colors.Lime;//おk
-                myDataList.MyDatasList[0].Color = Colors.LightBlue;//おk
                 Palette1.PaletteColors[0] = Colors.LimeGreen;
             };
 
@@ -71,72 +64,24 @@ namespace _20190312
             {
                 PaletteV2.SetColorList(MakeColorList(10));
             };
+            MyButton6.Click += (s, e) =>
+            {
+                PaletteV2.SetColorList(MakeColorList(256));
+            };
         }
 
         private void MyAdd()
         {
-            var rand = new Random();
-            var rgb = new byte[3];
 
-
-            Binding bind;
-            TextBlock tb;
-            StackPanel sp = new StackPanel() { Orientation = Orientation.Horizontal };
-            MyStackPanel.Children.Add(sp);
             List<Color> colors = MakeColorList(ColorCount);
-            for (int i = 0; i < ColorCount; i++)
-            {
-                rand.NextBytes(rgb);
-                myData.Add(new MyData() { Color = colors[i] });
-                //sp.Children.Add(new Border() { Width = 16, Height = 16 });
-
-                tb = new TextBlock() { Margin = new Thickness(1) };
-                tb.MouseLeftButtonDown += Tb_MouseLeftButtonDown;
-                sp.Children.Add(tb);
-
-                //bind = new Binding("Color");
-                bind = new Binding(nameof(MyData.Color));
-                bind.Source = myData[i];
-                tb.SetBinding(TextBlock.TextProperty, bind);
-
-                bind = new Binding(nameof(MyData.Color));
-                bind.Source = myData[i];
-                bind.Mode = BindingMode.TwoWay;
-                bind.Converter = new MyConverter();
-                tb.SetBinding(TextBlock.BackgroundProperty, bind);
-
-            }
-
-
-
-            colors = MakeColorList(ColorCount);
-            sp = new StackPanel() { Orientation = Orientation.Horizontal };
-            MyStackPanel.Children.Add(sp);
-            myDataList = new MyDatas(colors);
-            for (int i = 0; i < ColorCount; i++)
-            {
-                tb = new TextBlock() { Margin = new Thickness(1) };
-                tb.MouseLeftButtonDown += Tb_MouseLeftButtonDown;
-                bind = new Binding();
-                bind.Source = myDataList.MyDatasList[i];
-                bind.Path = new PropertyPath(MyData.ColorProperty);
-                tb.SetBinding(TextBlock.TextProperty, bind);
-                sp.Children.Add(tb);
-
-                bind = new Binding();
-                bind.Source = myDataList.MyDatasList[i];
-                bind.Path = new PropertyPath(MyData.ColorProperty);
-                bind.Mode = BindingMode.TwoWay;
-                bind.Converter = new MyConverter();
-                tb.SetBinding(TextBlock.BackgroundProperty, bind);
-
-            }
 
             Palette1 = new Palette(colors);
             MyStackPanel.Children.Add(Palette1);
 
             PaletteV2 = new PaletteVer2(colors);
             MyStackPanel.Children.Add(PaletteV2);
+
+            MyStackPanel.Children.Add(new PaletteVer2(colors));
 
         }
 
@@ -165,34 +110,6 @@ namespace _20190312
         }
     }
 
-    public class MyData : DependencyObject
-    {
-        //public Color Color { get; set; }
-        public Color Color
-        {
-            get { return (Color)GetValue(ColorProperty); }
-            set { SetValue(ColorProperty, value); }
-        }
-
-        // Using a DependencyProperty as the backing store for ColorProperty.  This enables animation, styling, binding, etc...
-        public static readonly DependencyProperty ColorProperty =
-            DependencyProperty.Register(nameof(Color), typeof(Color), typeof(MyData));
-
-    }
-
-    public class MyDatas
-    {
-        public List<MyData> MyDatasList { get; set; }
-        public MyDatas(List<Color> colors)
-        {
-            MyDatasList = new List<MyData>();
-            for (int i = 0; i < colors.Count; i++)
-            {
-                MyDatasList.Add(new MyData() { Color = colors[i] });
-            }
-        }
-    }
-
 
     //256colors
     public class PaletteVer2 : StackPanel
@@ -200,57 +117,63 @@ namespace _20190312
         //List<Border> Pans;
         private List<Color> AvailablePaletteColors;//実際に使う色リスト、減色処理で使う
         private ObservableCollection<Color> PaletteColors;//表示に使う色リスト(256色以下のとき透明色を含んでいる)
-        private List<Border> MyBorderPan;
+        private List<Border> PalettePans;
 
         public PaletteVer2(List<Color> colorList)
         {
             this.Orientation = Orientation.Horizontal;
-            
+            this.Margin = new Thickness(1);
             PaletteColors = new ObservableCollection<Color>();
-            MyInitialize();
+
+            AddPans();
 
             SetColorList(colorList);
-            for (int i = 0; i < colorList.Count; i++)
+            this.DataContext = PaletteColors;
+        }
+
+        private void AddPans()
+        {
+            var btn = new Button() { Content = "実行" };
+            btn.Click += Btn_Click;
+            this.Children.Add(btn);
+
+            PalettePans = new List<Border>();
+            for (int i = 0; i < 256; i++)
             {
+                //MakePans
+                PaletteColors.Add(Colors.Transparent);
+                Border bo = new Border()
+                {
+                    Width = 18,
+                    Height = 18,
+                    //Margin = new Thickness(1,0,1,0),
+                    BorderBrush = Brushes.AliceBlue,
+                    BorderThickness = new Thickness(1),
+                };
+                bo.MouseLeftButtonDown += Bo_MouseLeftButtonDown;
+                PalettePans.Add(bo);
+                this.Children.Add(bo);
+
+                //Binding
                 var bind = new Binding();
                 //bind.Source = PaletteColors;//this.Datacontext = PaletteColorsでも同じ結果
                 bind.Path = new PropertyPath("[" + i + "]");//無理やりすぎる
                 bind.Converter = new MyConverter();
                 bind.Mode = BindingMode.TwoWay;
-                MyBorderPan[i].SetBinding(Border.BackgroundProperty, bind);
+                bo.SetBinding(Border.BackgroundProperty, bind);
 
             }
-            this.DataContext = PaletteColors;
         }
 
-        private void MyInitialize()
+        private void Btn_Click(object sender, RoutedEventArgs e)
         {
-
-            MyBorderPan = new List<Border>();
-            for (int i = 0; i < 256; i++)
-            {
-                PaletteColors.Add(Colors.Transparent);
-                Border bo = new Border()
-                {
-                    Width = 16,
-                    Height = 16,
-                    Margin = new Thickness(2),
-                    BorderBrush = Brushes.AliceBlue,
-                    BorderThickness = new Thickness(1),
-                };
-                bo.MouseLeftButtonDown += Bo_MouseLeftButtonDown;
-                MyBorderPan.Add(bo);
-                this.Children.Add(bo);
-            }
+            MessageBox.Show("");
         }
+
         private void ClealPaletteColors()
         {
             for (int i = 0; i < 256; i++) { PaletteColors[i] = Colors.Transparent; }
         }
-        //private void ClealBindings()
-        //{
-        //    for (int i = 0; i < 256; i++) { MyBorderPan[i].SetBinding(Border.BackgroundProperty, new Binding()); }
-        //}
 
         //ColorListの入れ替え
         //相互Bindingしている背景色とPaletteColorは、どちらも指定していないと#00FFFFFFとTransparentになる
@@ -262,7 +185,7 @@ namespace _20190312
             ClealPaletteColors();//すべてTransparent
 
             for (int i = 0; i < colors.Count; i++)
-            {                
+            {
                 PaletteColors[i] = colors[i];
             }
         }
@@ -295,7 +218,7 @@ namespace _20190312
                 {
                     Width = 16,
                     Height = 16,
-                    Margin = new Thickness(2),
+                    Margin = new Thickness(1),
                     BorderBrush = Brushes.AliceBlue,
                     BorderThickness = new Thickness(1)
                 };
