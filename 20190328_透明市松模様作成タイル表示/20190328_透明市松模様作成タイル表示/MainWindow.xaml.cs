82201e7aa77dd4d5f493d37150b481320444263e
waten <gogowaten@yahoo.co.jp>
2019-03-28 21:14:25 +0900

    201903282114

---
+++ 20190328_透明市松模様作成タイル表示/20190328_透明市松模様作成タイル表示/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _20190328_透明市松模様作成タイル表示
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+            var bitmap = MakeTransparentChecered(10, Colors.LightGray);
+            MyGrid.Background = MakeTileBrush(bitmap);
+        }
+
+        /// <summary>
+        /// 市松模様画像作成
+        /// </summary>
+        /// <param name="cellSize">タイル1辺のサイズ</param>
+        /// <param name="gray">白じゃない方の色指定</param>
+        /// <returns></returns>
+        private WriteableBitmap MakeTransparentChecered(int cellSize, Color gray)
+        {
+            int width = cellSize * 2;
+            int height = cellSize * 2;
+            var wb = new WriteableBitmap(width, height, 96, 96, PixelFormats.Rgb24, null);
+            int stride = wb.Format.BitsPerPixel / 8 * width;
+            byte[] pixels = new byte[stride * height];
+            int p = 0;
+            Color iro;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if ((y < cellSize & x < cellSize) | (y >= cellSize & x >= cellSize))
+                    {
+                        iro = Colors.White;
+                    }
+                    else { iro = gray; }
+
+                    p = y * stride + x * 3;
+                    pixels[p] = iro.R;
+                    pixels[p + 1] = iro.G;
+                    pixels[p + 2] = iro.B;
+                }
+            }
+            wb.WritePixels(new Int32Rect(0, 0, width, height), pixels, stride, 0);
+            return wb;
+        }
+
+        /// <summary>
+        /// Bitmapからブラシ作成
+        /// 引き伸ばし無しでタイル状に敷き詰め
+        /// </summary>
+        /// <param name="bitmap"></param>
+        /// <returns></returns>
+        private ImageBrush MakeTileBrush(BitmapSource bitmap)
+        {
+            var imgBrush = new ImageBrush(bitmap);
+            imgBrush.Stretch = Stretch.None;//これは必要ないかも
+            //タイルモード、タイル
+            imgBrush.TileMode = TileMode.Tile;
+            //タイルサイズは元のサイズ
+            imgBrush.Viewport = new Rect(0, 0, bitmap.PixelWidth, bitmap.PixelHeight);
+            //タイルサイズ指定は絶対値、これで引き伸ばしなしになる
+            imgBrush.ViewportUnits = BrushMappingMode.Absolute;
+            return imgBrush;
+        }
+
+    }
+}
