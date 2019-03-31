239623ffb2b8184e2be351538c0f9521f51fedd3
waten <gogowaten@yahoo.co.jp>
2019-03-31 17:24:37 +0900

    201903311724 blog

--- 20190331_画像埋め込み/20190331_画像埋め込み/MainWindow.xaml.cs
+++ 20190331_画像埋め込み/20190331_画像埋め込み/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System.Windows;
 using System.Windows.Media.Imaging;
+//WPF、アプリの実行ファイルに画像ファイルを埋め込んで、それを取り出して表示するまでの手順メモ(ソフトウェア ) - 午後わてんのブログ - Yahoo!ブログ
+//https://blogs.yahoo.co.jp/gogowaten/15919321.html
 
 namespace _20190331_画像埋め込み
 {
