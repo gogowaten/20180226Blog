04a8a6a1c10cfa3628d9813c37f10c06c8280f0e
waten <gogowaten@yahoo.co.jp>
2019-04-18 12:16:20 +0900

    201904181216

--- 20190418_画像のエッジ抽出/20190418_画像のエッジ抽出/MainWindow.xaml.cs
+++ 20190418_画像のエッジ抽出/20190418_画像のエッジ抽出/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MyNumericUpDownInteger;
 
 namespace _20190418_画像のエッジ抽出
 {
@@ -23,6 +24,45 @@ namespace _20190418_画像のエッジ抽出
         public MainWindow()
         {
             InitializeComponent();
+
+            NumericUDInteger nu;
+            for (int i = 0; i < 3; i++)
+            {
+                var st = new StackPanel();
+                st.Orientation = Orientation.Horizontal;
+                MyStackPanel.Children.Add(st);
+                for (int f = 0; f < 3; f++)
+                {
+                    nu = new NumericUDInteger();
+                    nu.Width = 50;
+                    nu.Max = 255;
+                    nu.Min = -255;
+                    st.Children.Add(nu);
+                }
+            }
+            nu = new NumericUDInteger();
+            nu.Max = 255;
+            nu.Min = -255;
+            
+            MyStackPanel.Children.Add(nu);
+            
+        }
+
+
+
+
+
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            int aa = Panel.GetZIndex(MyImage);
+            Panel.SetZIndex(MyImageOrigin, aa + 1);
+        }
+
+        private void Grid_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            int aa = Panel.GetZIndex(MyImage);
+            Panel.SetZIndex(MyImageOrigin, aa - 1);
         }
     }
 }
